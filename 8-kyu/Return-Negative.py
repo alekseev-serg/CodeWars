8b86cc71de985edd1f194b34616d683236650443
sergey.alekseev <demiurg58@gmail.com>
2025-05-04 09:34:05 +0300

    do 4 lessons

---
+++ 8-kyu/Return-Negative.py
@@ -0,0 +1,7 @@
+# def make_negative(number):
+#     if (number <= 0): return number
+#     if (number > 0): return number * (-1)
+#
+
+def make_negative(number):
+    return -number if number > 0 else number
